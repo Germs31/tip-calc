25946c333c86982471f55fa23d3863caa40e2e68
German Nunez <germannunez@Germans-MacBook-Pro.local>
2019-09-30 17:28:46 -0700

    functionality working

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('hello')
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    const bill = document.querySelector("#bill").value
+    const percent = document.querySelector("#percentage").value
+    let sum = parseFloat(bill) * (parseFloat(percent)/ 100)
+    let display = document.querySelector("#result")
+    return display.innerHTML = `You should leave $${sum.toFixed(2)} as a tip`
+});
+
